de7808a8e62c0e1343ef8c09d5f0aa1ca2d85f82
Marcel Hecko <maco@blava.net>
2012-09-23 01:56:59 +0200

    understands groupd and api moved to PHPZabbixApi

--- index.php
+++ index.php
@@ -1,80 +1,133 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<meta http-equiv="refresh" content="5">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- <meta http-equiv="refresh" content="5"> -->
+<style>
+.btn {
+	font-size: .9em;
+	width: 7em;
+	height: 7em;
+	margin-bottom: .3em;
+	margin-right: .3em;
+}
+</style>
 </head>
 <body>
-<div class=container>
+<div class=container id=content>
+	<script src="jquery/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
 	<div class=row>
-		<h2>Status of Zabbix IT Services</h2><h4>@ <?php echo date("H:i"); ?> <a href=""><i class="icon-refresh"></i></a></h4>
+		<h4><a href=""><i class="icon-refresh"></i></a> <?php echo date("H:i:s"); ?></h4>
 		<hr>
 	</div>
+	<div class=row>
 <?php
 
 error_reporting(0);
 
-//define names of services here
-$s = array('ATM','My secret service','Selenium','Pinger');
+$s = array(
+	'PROD' => array(
+		'ATM' => 'Starame sa o vase peniazky',
+		'ATM',
+		'Pergola',
+		'My secret service',
+		'ATM',
+                'My secret service',
+		'Pinger',
+		'Selenium'
+		),
+	'UAT2' => array(
+		'Selenium',
+		'Pinger'
+		));
 
-require("ZabbixAPI.class.php");
+require("PhpZabbixApi_Library/ZabbixApi.class.php");
 
-// This enables debugging, this is rather verbose but can help debug problems
-ZabbixAPI::debugEnabled(TRUE);
+$api = new ZabbixApi('http://localhost/zabbix/api_jsonrpc.php', 'admin', 'zabbix');
 
-// This logs into Zabbix, and returns false if it fails
-ZabbixAPI::login('http://localhost/zabbix/','admin','zabbix')
-	or die('Unable to login: '.print_r(ZabbixAPI::getLastError(),true));
-
-// This gets the version of the zabbix server
-#$version = ZabbixAPI::fetch_string('apiinfo','version')
-#	or die('Unable to get Zabbix Version: '.print_r(ZabbixAPI::getLastError(),true));
-#
-#echo "Server running Zabbix API Version: $version\n<br><hr>";
-#
-//$services = ZabbixAPI::fetch('service','get',array('output'=>'extend','filter'=>array('name'=>'new')))
-foreach ($s as $name) {
-	$stat = ZabbixAPI::fetch_row('service','get',array('output'=>'extend','filter'=>array('name'=>"$name")))
-		or die(print_r(ZabbixAPI::getLastError(),true));
-	//print_r($stat);
-	$stats[$name] = $stat;
+$group=0;
+$i=0;
+foreach ($s as $gname=>$g) {
+	$group++;
+	foreach ($g as $name=>$comment) {
+		$i++;
+		if (is_numeric($name)) { $name = $comment; $comment = 'no comment'; };
+		$stat = $api->serviceGet(array(
+			//'selectAlarms'=>TRUE,
+			//'selectTrigger'=>TRUE,
+			//'selectDependencies'=>TRUE,
+			'output'=>'extend',
+			'filter'=>array('name'=>"$name")));
+		if ($stat[0]->status!="") {
+			$groups[$gname][$name.$i] = $stat[0];
+			$groups[$gname][$name.$i]->comment = $comment;
+			$groups[$gname][$name.$i]->group_name = $gname;
+			$comment = '';
+		}
+	}
 }
 
-//print everything our
+//echo '<pre>';
+//print_r($groups);
+
+//print everything out
 $i = 0;
-print '<div class="row"><div class="span12">';
-$button_style = 'height:100px; width:100px';
-foreach ($stats as $service) {
-	//status value is linked to trigger severity
-	echo '<a href="/zabbix/srv_status.php">';
-	$i++;
-	if ($service["status"] >= 4) {
-		echo '<button class="btn btn-large btn-danger" style="'.$button_style.'" type=button>'.
-			'<p>'.$service['name'].'</p><i class="icon-fire"></i>'.
-			'</button>';
-
-	} elseif ($service["status"] <= 3 & $service["status"] != 0) {
-		echo '<button class="btn btn-large btn-warning" style="'.$button_style.'" type=button>'.
-                        '<p>'.$service['name'].'</p><i class="icon-exclamation-sign"></i>'.
-                        '</button>';
-	} else {
-		echo '<button class="btn btn-large btn-success" style="'.$button_style.'" type=button>'.
-                        '<p>'.$service['name'].'</p><i class="icon-thumbs-up"></i>'.
-                        '</button>';
+foreach ($groups as $gname=>$stats) {
+	echo '<h5>'.$gname.'</h5>';
+	foreach ($stats as $service) {
+		//status value is linked to trigger severity
+		$i++;
+		//echo '<div class="span2">';
+		if ($service->status >= 4) {
+			echo '<button class="btn btn-large btn-danger" type=button '.
+				'data-trigger=hover rel="popover" data-placement=right '.
+				'data-content="'.$service->comment.'">'.
+				$service->name.
+				'</button>';
+	
+		} elseif ($service->status <= 3 & $service->status != 0) {
+			echo '<button class="btn btn-large btn-warning" type=button '.
+				'data-trigger=hover rel="popover" data-placement=right '.
+				'data-content="'.$service->comment.'">'.
+				$service->name.
+				'</button>';
+		} else {
+			echo '<button class="btn btn-large btn-success" type=button '.
+				'data-trigger=hover rel="popover" data-placement=right '.
+				'data-content="'.$service->comment.'">'.
+				$service->name.
+				'</button>';
+		}
+		//echo '</div>'."\n";
+		//7 6 items per line - reflected in span2, as 2*6=12 and that is how it should be for Twitter Bootstrap
+		if ($i >= 10) {
+			echo '</div><div class=row>';
+			$i = 0;
+		}
 	}
-	echo '</a>'."\n";
-	if ($i >= 3) {
-		echo '</div></div><div class=row><div class="span12"><br>';
-		$i = 0;
-	}
+	$i=0;
+	//starting a new group
+	echo '</div><div class=row>'."\n";
 }
-echo '</div>';
-
 ?>
 	</div>
+
 	<div class=container><hr>
 		<small>Build by Marcel Hecko, 2012</small>
 	</div>
+	<script type="text/javascript">  
+		$('.btn').live("mouseover", function() { $(this).popover('show'); });
+	</script>
 </div>
+
+<!-- refresh part of the page -->
+<script>
+setInterval(function(){
+	$('.btn').popover('hide').popover('disable');
+	$("#content").load("index.php #content");
+}, 10000);
+</script>
+
 </body>
 </html>
